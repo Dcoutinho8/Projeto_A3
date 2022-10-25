14fbc1a4fcff7371d00d154c7e920691f49eb4a1
Dcoutinho8 <danilocoutti08@gmail.com>
2022-10-25 02:19:42 -0300

    novas classes

---
+++ src/main/java/com/mycompany/projeto_a3/AlergiaMed.java
@@ -0,0 +1,13 @@
+package com.mycompany.projeto_a3;
+
+public class AlergiaMed {
+    private int nomeRemedio;
+
+    public int getNomeRemedio() {
+        return nomeRemedio;
+    }
+
+    public void setNomeRemedio(int nomeRemedio) {
+        this.nomeRemedio = nomeRemedio;
+    }
+}
---
+++ src/main/java/com/mycompany/projeto_a3/MedUsoContinuo.java
@@ -0,0 +1,24 @@
+package com.mycompany.projeto_a3;
+
+public class MedUsoContinuo {
+    private int nomeRemedio;
+    private int horarioMed;
+
+    public int getHorarioMed() {
+        return horarioMed;
+    }
+
+    public void setHorarioMed(int horarioMed) {
+        this.horarioMed = horarioMed;
+    }
+
+    public int getNomeRemedio() {
+        return nomeRemedio;
+    }
+
+    public void setNomeRemedio(int nomeRemedio) {
+        this.nomeRemedio = nomeRemedio;
+    }
+    
+    
+}
